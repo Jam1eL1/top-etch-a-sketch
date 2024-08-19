f5f6ce6f473bc70b373e4d7d73e8512ef8dffca2
Jam1eL1 <23jisu@gmail.com>
2024-08-19 15:31:03 -0700

    feat(grid): create 16x16 default grid with dynamic sizing

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('.grid-container');
+// create divs for 16x16 grid under the  `container`
+// same style applie to each div - hover effect - use event listeners
+// create a diff class with the hover effect styles, and simply 'add' style to the div when hover effect is applied
+
+function createGrid(size) {
+    container.innerHTML = '';
+    const itemSize = 600 / size;
+
+    for (let i = 0; i < size * size; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+        gridItem.style.width = `${itemSize}px`;
+        gridItem.style.height = `${itemSize}px`;
+        container.appendChild(gridItem);
+    }
+}
+
+createGrid(16);
